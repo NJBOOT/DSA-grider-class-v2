2b7e3ef7b0f9ad0c8e6e7949599aadd6bbea1b5e
Nico Barbarese <nicobarbarese@gmail.com>
2021-02-08 11:46:25 -0500

    reverseint

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,17 +8,15 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-// MY SOLUTION
-function reverseInt(n) {
-  const arr = Math.abs(n).toString().split("");
-  for (let i = 0; i < Math.floor(arr.length / 2); i++) {
-    let temp = arr[i];
-    arr[i] = arr[arr.length - i - 1];
-    arr[arr.length - i - 1] = temp;
+const reverseInt = n => {
+  let num = Math.abs(n).toString().split("");
+  for (let i = 0; i < ~~num.length / 2; i++) {
+    let temp = num[num.length - 1 - i];
+    num[num.length - 1 - i] = num[i];
+    num[i] = temp;
   }
-  const flipped = +arr.join("");
-  return n < 0 ? -flipped : flipped;
-}
+  return +num.join("") * Math.sign(n);
+};
 
 module.exports = reverseInt;
 
@@ -26,4 +24,16 @@ module.exports = reverseInt;
 // function reverseInt(n) {
 //   const flipped = parseInt(n.toString().split("").reverse().join(""));
 //   return flipped * Math.sign(n);
+// }
+
+// MY SOLUTION
+// function reverseInt(n) {
+//   const arr = Math.abs(n).toString().split("");
+//   for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+//     let temp = arr[i];
+//     arr[i] = arr[arr.length - i - 1];
+//     arr[arr.length - i - 1] = temp;
+//   }
+//   const flipped = +arr.join("");
+//   return n < 0 ? -flipped : flipped;
 // }
