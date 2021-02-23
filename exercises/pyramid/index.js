3ababccc914fb5d021e77a6161211407ff27ece6
Nico Barbarese <nicobarbarese@gmail.com>
2021-02-23 13:49:22 -0500

    pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,25 +14,32 @@
 //       ' ### '
 //       '#####'
 
-// Recursive
-function pyramid(n, row = 0, level = "") {
-  let levelLength = n * 2 - 1;
-  let midLength = Math.floor(levelLength / 2);
-  if (n === row) {
-    return;
+const pyramid = N => {
+  let length = N * 2 - 1;
+  let midLength = ~~(length / 2);
+
+  for (let row = 0; row < N; row++) {
+    let step = "";
+    for (let col = 0; col < length; col++) {
+      if (col >= midLength - row && col <= midLength + row) {
+        step += "#";
+      } else {
+        step += " ";
+      }
+    }
+    console.log(step);
   }
-  if (level.length === levelLength) {
-    console.log(level);
-    return pyramid(n, row + 1);
+};
+
+const pyramidTWO = N => {
+  let row = 1;
+  while (row <= N) {
+    let step = "";
+    step = " ".repeat(N - row) + "#".repeat(row * 2 - 1) + " ".repeat(N - row);
+    console.log(step);
+    row++;
   }
-  let add;
-  if (midLength - row <= level.length && midLength + row >= level.length) {
-    add = "#";
-  } else {
-    add = " ";
-  }
-  return pyramid(n, row, level + add);
-}
+};
 
 module.exports = pyramid;
 
@@ -53,3 +60,38 @@ module.exports = pyramid;
 //       console.log(level);
 //     }
 //   }
+
+// function pyramid(n, row = 0, level = "") {
+//   let levelLength = n * 2 - 1;
+//   let midLength = Math.floor(levelLength / 2);
+//   if (n === row) {
+//     return;
+//   }
+//   if (level.length === levelLength) {
+//     console.log(level);
+//     return pyramid(n, row + 1);
+//   }
+//   let add;
+//   if (midLength - row <= level.length && midLength + row >= level.length) {
+//     add = "#";
+//   } else {
+//     add = " ";
+//   }
+//   return pyramid(n, row, level + add);
+// }
+
+// const pyramidAlt = N => {
+//   let length = N * 2 - 1;
+//   let midPoint = ~~(length / 2);
+//   let row = 0;
+//   while (row < N) {
+//     let step = new Array(length).fill(" ");
+//     for (let col = 0; col < length; col++) {
+//       if (col >= midPoint - row && col <= midPoint + row) {
+//         step[col] = "#";
+//       }
+//     }
+//     row++;
+//     console.log(step.join(""));
+//   }
+// };
